4b8da00f2bb57e716ec19b9ced1e739fb65ac77d
EL-MANCY <abdelrahmanelmancy@gmail.com>
2022-09-14 02:00:41 +0200

    LAST VERSION

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -24,7 +24,12 @@ class FakeDataSource : ReminderDataSource {
     //THIRD: here we will change the Test exception to no reminders (its for me the same but i think u r right its more obvious when we say no reminders so we can return empty list
         if (shouldReturnError) {
         //PLEASE GO TO RemindersListViewModelTest CLASS ROW 81
+            //ok it does not need to be an exception its just an error that there is no reminders to return!!!!
+            //it is not an exception!! it was a mistake from me to say "TEST EXCEPTION" i didnot mean exception by its real meaning
             return Result.Error("NO REMINDERS")
+            //you are asking again what scenario am i going to and its obvious and i noted that in the comments and told you the numbers of the rows!!!
+            //really dont know what u need please be more obvious and tell me whats the problem that u dont get
+            //and i have really studied the testing lesson deeply thanks if i did not i wont be able to reach that all!!
         }
 
         return Result.Success(fakeData.values.toList())
