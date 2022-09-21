e2f14e77004365bbabddc2ce45c0c119c4e42ea3
EL-MANCY <abdelrahmanelmancy@gmail.com>
2022-09-21 15:32:19 +0200

    testing

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -6,14 +6,13 @@ import java.util.LinkedHashMap
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
-  //  "Why should we handle errors" , ans:After throwing an exception, an exception handler must be found to handle the exception, or the app will terminate thats why we should handle errors for any unknown error
 
-// ITS NAME IS shouldReturnError last time it was isEmpty as it was returning an empty list and that was wrong so i was in a hurry so i named it isError
-//its used in the condition which we excpect an error from the FakeDataSource as a Error Handling method
+ //its used if any exception with the database happened while fetching the data from it as maybe something unknown happened to the schema
     private var shouldReturnError = false
-    // i forgot to change setReturnEmpty to setReturnError as previous i was returning an empty list but its used for error handling anyways if the repo is empty or not
+    //    //here in the fake data source we need to send the error by our self as its a fake data as we cant use the real schema in testig as the user dont need it so we do it by ourself
     fun setReturnError(value: Boolean) {
         //setReturnError function is used to make the fakedatasource return errors and to check it as its implemented in the ReminderListViewModelTest Class ROW NO. 83
+        //we can say its like a flag or something that used to sent an error
         shouldReturnError = value
     }
 
