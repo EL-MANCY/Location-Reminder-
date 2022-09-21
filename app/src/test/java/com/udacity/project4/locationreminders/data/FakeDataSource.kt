d761e5674c323435230c0faa2b7e9ba5ab3d8878
EL-MANCY <abdelrahmanelmancy@gmail.com>
2022-09-21 15:39:00 +0200

    testing

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -6,8 +6,9 @@ import java.util.LinkedHashMap
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
+    //All my code was from the 3 testing lessons in the course and i made it by myself
 
- //its used if any exception with the database happened while fetching the data from it as maybe something unknown happened to the schema
+ //its used if any exception with the database happened while fetching the data from it as maybe something unknown happened to the schema so we should catch the exception and send it to the viewmodel
     private var shouldReturnError = false
     //    //here in the fake data source we need to send the error by our self as its a fake data as we cant use the real schema in testig as the user dont need it so we do it by ourself
     fun setReturnError(value: Boolean) {
@@ -15,7 +16,7 @@ class FakeDataSource : ReminderDataSource {
         //we can say its like a flag or something that used to sent an error
         shouldReturnError = value
     }
-
+//
     private val fakeData: LinkedHashMap<String, ReminderDTO> = LinkedHashMap()
 //Return the reminders
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
